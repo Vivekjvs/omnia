8cb3bcdc46eb515b87e06a4c18efcb6121a0ff54
Md Zikrullah <mohmmedzikrullah159@gmail.com>
2022-05-08 22:40:45 +0530

    adding user profile

--- database.py
+++ database.py
@@ -5,9 +5,9 @@ def connectdatabase():
   #connectiong to the database
   mydb = mysql.connector.connect(
     host="localhost",
-    user="srikar23",
-    port = 3305,
-    password="Srikar@23",
+    user="root",
+    port = 3306,
+    password="4321",
     database = "codingprofilesfeedback"
   )
   #creating a cursor to execute our queries
